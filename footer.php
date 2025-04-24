2029385b27bb712317e42ab7b20b5d557fc13558
Instar-F <karmalimuhammed007@gmail.com>
2025-04-24 08:51:59 +0300

    Includes

---
+++ footer.php
@@ -0,0 +1,6 @@
+<hr>
+<footer>
+    <p>&copy; 2025 MatteProvGenerator</p>
+</footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>MatteProvGenerator</title>
+</head>
+<body>
+    <h1>MatteProvGenerator</h1>
+    <hr>
