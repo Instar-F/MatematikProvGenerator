7cb718a237d13e04b33cfa5f4414ad476201f0df
Instar-F <karmalimuhammed007@gmail.com>
2025-04-24 13:31:25 +0300

    Update header.php
    Removed unnecessary stuff, fixed some more

--- include/header.php
+++ include/header.php
@@ -18,33 +18,32 @@ if(isset($_GET['logout']) && $_GET['logout'] == "true"){
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <link rel="stylesheet" href="style.css">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-<script defer src="js/script.js"></script>
+<!-- <script defer src="script.js"></script> -->
 </head>
 <body>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active"  href="index.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="apointment.php">add apointment</a>
-        </li>
-      </ul>
-      <?php if((isset($_SESSION['user']['id'])) && $user_obj->checkLoginStatus($_SESSION['user']['id'])): ?> 
-      
-      <form action="" class="d-flex" method="get" role="search">
-        <button class="btn btn-outline-danger" name="logout" value="true" type="submit">Log out</button>
-      </form>
 
-      <?php endif ?>
-    
+<header>
+    <nav class="navbar navbar-expand-lg navbar-dark py-4" style="background-color: #e0040e;">
+        <div class="container">
+            <a class="navbar-brand" href="#" style="font-size: 2em;">Axxell Ab</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-    </div>
-  </div>
-</nav>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/contact">Contact</a>
+                    </li>
+                </ul>
+
+                <?php if ((isset($_SESSION['user']['id'])) && $user_obj->checkLoginStatus($_SESSION['user']['id'])): ?>
+                <form action="" class="d-flex" method="get" role="search">
+                    <button class="btn btn-outline-light" name="logout" value="true" type="submit">Log out</button>
+                </form>
+                <?php endif ?>
+            </div>
+        </div>
+    </nav>
+</header>
